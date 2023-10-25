53034b3a67fb42dbf719b8f1a8d37d03257a42c8
manojconcept <manojveerapavu@gmail.com>
2023-10-25 22:17:17 +0530

    Day 5

---
+++ For the given JSON iterate over all for loops (for,for in,for of,forEach).js
@@ -0,0 +1,70 @@
+// For the given JSON iterate over all for loops (for,for in,for of,forEach)
+let getData = {
+    "name": "John Doe",
+    "title": "Software Engineer",
+    "contact": {
+      "email": "john.doe@example.com",
+      "phone": "+1 (555) 555-5555",
+      "linkedin": "www.linkedin.com/in/johndoe",
+      "github": "www.github.com/johndoe"
+    },
+    "summary": "Experienced software engineer with a passion for developing innovative solutions. Skilled in full-stack web development and experienced in agile methodologies. Strong problem-solving abilities and a quick learner.",
+    "education": [
+      {
+        "degree": "Bachelor of Science in Computer Science",
+        "university": "XYZ University",
+        "graduation_year": 2018
+      }
+    ],
+    "experience": [
+      {
+        "position": "Software Engineer",
+        "company": "TechCo",
+        "location": "San Francisco, CA",
+        "start_date": "June 2018",
+        "end_date": "Present",
+        "description": "Develop and maintain features for a large-scale web application. Collaborate with cross-functional teams to design and implement new features. Utilize technologies including JavaScript, React, Node.js, and MongoDB."
+      },
+      {
+        "position": "Intern",
+        "company": "ABC Software",
+        "location": "New York, NY",
+        "start_date": "May 2017",
+        "end_date": "August 2017",
+        "description": "Assisted in the development of front-end features and participated in code reviews. Gained experience with HTML, CSS, and JavaScript."
+      }
+    ],
+    "skills": [
+      "JavaScript",
+      "React",
+      "Node.js",
+      "HTML/CSS",
+      "MongoDB",
+      "Agile",
+      "Git",
+      "Python"
+    ],
+    "languages": [
+      "English",
+      "Spanish"
+    ],
+    "certifications": [
+      {
+        "title": "AWS Certified Developer - Associate",
+        "issuing_organization": "Amazon Web Services",
+        "year": 2020
+      },
+      {
+        "title": "Certified ScrumMaster",
+        "issuing_organization": "Scrum Alliance",
+        "year": 2019
+      }
+    ],
+    "publications": []
+  }
+  
+  let getInfoData = (getObj) => ({
+        for(let I in getObj){
+            
+        }
+  })
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Task 5
+1. For the given JSON iterate over all for loops (for,for in,for of,forEach).[link]()
+2. Create your own resume data in JSON format.[link]()
+3. Read about difference between window,screen and document in javascript.
+4. Codekata practice.
